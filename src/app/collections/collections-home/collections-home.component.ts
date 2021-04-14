13ead8377405016d37097cca7706ee270cf5010b
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-04-14 11:12:46 -0300

    table comp

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: "James", age: 24, job: "Designer" },
+    { name: "Jill", age: 26, job: "Engenieer" },
+    { name: "Alice", age: 22, job: "Engenieer" },
+  ]
+  headers = [
+    { key: "name", label: "Name" },
+    { key: "age", label: "Age" },
+    { key: "job", label: "Job" },
 
+  ]
   constructor() { }
 
   ngOnInit(): void {
